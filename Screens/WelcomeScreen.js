46cc666970f1636c3437e2a66b723d43fbbdabac
HM07 <hmimtiaz2@gmial.com>
2024-04-01 02:35:53 +0600

    gsap added

--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -1,11 +1,15 @@
 import React, { useRef, useEffect } from 'react';
-import { View, StyleSheet, Text, Animated } from 'react-native';
+import { View, StyleSheet, Text, Animated, Image } from 'react-native';
 import Background from './Background';
 import Btn from './Btn';
 import { darkGreen, green } from './Constants';
+import { gsap, Back } from 'gsap-rn';
+import logoimage from '../assets/logo1.jpg';
+
 
 const WelcomeScreen = (props) => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
+  const logoRef = useRef(null);
 
   useEffect(() => {
     const fadeIn = Animated.timing(
@@ -34,9 +38,17 @@ const WelcomeScreen = (props) => {
     return () => loop.stop();
   }, [fadeAnim]);
 
+
+  useEffect(() => {
+    const logo = logoRef.current;
+    gsap.to(logo, { duration: 3, transform: { rotate: 360, scale: 1 }, ease: Back.easeInOut,  repeat: -1,
+    repeatDelay: 3, });
+  }, []);
+
   return (
     <Background>
       <View style={{ marginHorizontal: 40, marginVertical: 100 }}>
+        <Image ref={logoRef} style={styles.logo} source={logoimage} />
         <Animated.Text style={[styles.appName, { opacity: fadeAnim }]}>TutorLinkHUB</Animated.Text>
         <Animated.Text style={[styles.appDescription, { opacity: fadeAnim }]}>Tuition Management App</Animated.Text>
         <Btn bgColor={green} textColor='white' btnLabel="Login" Press={() => props.navigation.navigate("LoginScreen")} />
@@ -51,8 +63,9 @@ const styles = StyleSheet.create({
     color: 'yellow',
     fontSize: 43,
     fontWeight: 'bold',
-    marginTop: 90,
-    textAlign: 'center'
+    marginTop: 30,
+    textAlign: 'center',
+    fontStyle: 'italic',
   },
   appDescription: {
     color: 'lightblue',
@@ -60,7 +73,14 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 90,
     textAlign: 'center'
-  }
+  },
+  logo: {
+    alignSelf: 'center',
+    height: 100,
+    width: 100,
+    marginBottom: 10,
+    marginTop: 40
+  },
 });
 
 export default WelcomeScreen;
